b2c6aaa485664a1a8c13ea76fa16280bcade856b
zhangchao <544262408@qq.com>
2023-03-02 15:42:33 +0800

    feat: 🎸 add web3 mork

--- packages/web/cypress/e2e/spec.cy.ts
+++ packages/web/cypress/e2e/spec.cy.ts
@@ -5,6 +5,20 @@ describe('App First Test', () => {
     cy.contains('Welcome to the Multichain - SMPC')
     cy.contains('Create new Wallet')
     cy.contains('Support Network')
+
+    cy.contains('Connect wallet').click()
+    cy.contains('Connect with MetaMask').click()
+    cy.contains('metamask')
+    cy.contains('Görli')
+    cy.contains('Create new Wallet').click()
+    cy.contains('Login account')
+    cy.contains('Set the node')
+    cy.get('.e2eloginselect').find("button").click()
+    cy.get('ul>li:nth-child(2)').click()
+    cy.get('button').contains('Login').click();
+    
+
+    
     
   })
 })
---
+++ packages/web/cypress/support/ethereum.ts
@@ -0,0 +1,84 @@
+/**
+ * Updates cy.visit() to include an injected window.ethereum provider.
+ */
+
+import { Eip1193Bridge } from '@ethersproject/experimental/lib/eip1193-bridge'
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { Wallet } from '@ethersproject/wallet'
+
+// todo: figure out how env vars actually work in CI
+// const TEST_PRIVATE_KEY = Cypress.env('INTEGRATION_TEST_PRIVATE_KEY')
+const TEST_PRIVATE_KEY = '0xe580410d7c37d26c6ad1a837bbae46bc27f9066a466fb3a66e770523b4666d19'
+
+// address of the above key
+const TEST_ADDRESS_NEVER_USE = new Wallet(TEST_PRIVATE_KEY).address
+
+const provider = new JsonRpcProvider('https://goerli.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847', 5)
+const signer = new Wallet(TEST_PRIVATE_KEY, provider)
+// import Web3 from 'web3';
+// const web3 = new Web3();
+
+export const injected = new (class extends Eip1193Bridge {
+  chainId = /* GOERLI= */ 5
+
+  async sendAsync(...args: any[]) {
+    console.debug('sendAsync called', ...args)
+    return this.send(...args)
+  }
+  async send(...args: any[]) {
+    console.debug('send called', ...args)
+    const isCallbackForm = typeof args[0] === 'object' && typeof args[1] === 'function'
+    let callback
+    let method
+    let params
+    if (isCallbackForm) {
+      callback = args[1]
+      method = args[0].method
+      params = args[0].params
+    } else {
+      method = args[0]
+      params = args[1]
+    }
+    if (method === 'eth_requestAccounts' || method === 'eth_accounts') {
+      if (isCallbackForm) {
+        callback({ result: [TEST_ADDRESS_NEVER_USE] })
+      } else {
+        return Promise.resolve([TEST_ADDRESS_NEVER_USE])
+      }
+    }
+    if (method === 'eth_chainId') {
+      if (isCallbackForm) {
+        callback(null, { result: '0x5' })
+      } else {
+        return Promise.resolve('0x5')
+      }
+    }
+    if(method === 'personal_sign'){
+      const data = await signer.signMessage(params[0])
+      console.log('params',params[0])
+      console.log('personal_sign',data)
+      if (isCallbackForm) {
+        callback(null, { result: data })
+      } else {
+        return Promise.resolve(data)
+      }
+    }
+    console.log('ethereum method',method)
+    try {
+      const result = await super.send(method, params)
+      console.debug('result received', method, params, result)
+      if (isCallbackForm) {
+        callback(null, { result })
+      } else {
+        return result
+      }
+    } catch (error) {
+      if (isCallbackForm) {
+        callback(error, null)
+      } else {
+        throw error
+      }
+    }
+  }
+})(signer, provider)
