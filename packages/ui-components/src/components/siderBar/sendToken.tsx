19c3c214f0eb6e880c76e638492f4cc426593bf6
zhangchao <544262408@qq.com>
2023-03-13 10:42:23 +0800

    feat: 🎸 add tx review

--- packages/ui-components/src/components/siderBar/sendToken.tsx
+++ packages/ui-components/src/components/siderBar/sendToken.tsx
@@ -1,13 +1,18 @@
-import { FC, useEffect } from 'react'
+import { FC, useCallback, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 import Avvvatars from 'avvvatars-react'
+import { When } from 'react-if'
+import ethlogo from '../../assets/icon/ethereum-logo.png'
+import ChainName from '../chainList/chainName'
 
 const SendToken: FC<{ open?: boolean; callBack: () => void }> = ({ open, callBack }) => {
   const [isTokenOpen, setIsTokenOpen] = useState(open || false)
+  const [isPreviewStep, setIsPreviewStep] = useState(false)
 
   function closeTokenModal() {
     setIsTokenOpen(false)
+    setIsPreviewStep(false)
     if (callBack) {
       callBack()
     }
@@ -17,6 +22,14 @@ const SendToken: FC<{ open?: boolean; callBack: () => void }> = ({ open, callBac
       setIsTokenOpen(open)
     }
   }, [open])
+
+  const next = useCallback(() => {
+    setIsPreviewStep(true)
+  }, [setIsPreviewStep])
+  const previous = useCallback(() => {
+    setIsPreviewStep(false)
+  }, [setIsPreviewStep])
+
   return (
     <>
       <Transition appear show={isTokenOpen} as={Fragment}>
@@ -44,73 +57,143 @@ const SendToken: FC<{ open?: boolean; callBack: () => void }> = ({ open, callBac
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                    Send Tokens
-                  </Dialog.Title>
-                  <div className="mt-4 flex flex-col  gap-1 ">
-                    <div className="mb-6">
-                      <label htmlFor="sendingfrom" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Sending from{' '}
-                      </label>
-                      <div className="flex flex-row ">
-                        <Avvvatars value={'0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab'} style="shape" size={40} />
-                        <div className="break-all pl-2">0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab</div>
-                      </div>
-                    </div>
-                    <div className="mb-6">
-                      <label htmlFor="assert" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Assert{' '}
-                      </label>
-                      <select
-                        id="assert"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      >
-                        <option selected>eth</option>
-                        <option value="bnb">bnb</option>
-                        <option value="btc">btc</option>
-                      </select>
-                    </div>
+                <div>
+                  <When condition={isPreviewStep == false}>
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 flex  flex-row justify-between">
+                        <span className=" flex-1 ">Send Tokens</span>{' '}
+                        <div className=" w-28 mx-2">
+                          <ChainName></ChainName>
+                        </div>
+                      </Dialog.Title>
+                      <div className="mt-4 flex flex-col  gap-1  w-96 ">
+                        <div className="mb-6">
+                          <label htmlFor="sendingfrom" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Sending from{' '}
+                          </label>
+                          <div className="flex flex-row ">
+                            <Avvvatars value={'0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab'} style="shape" size={40} />
+                            <div className="break-all pl-2">0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab</div>
+                          </div>
+                        </div>
+                        <div className="mb-6">
+                          <label htmlFor="assert" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Assert{' '}
+                          </label>
+                          <select
+                            id="assert"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          >
+                            <option selected>eth</option>
+                            <option value="bnb">bnb</option>
+                            <option value="btc">btc</option>
+                          </select>
+                        </div>
 
-                    <div className="mb-6">
-                      <label htmlFor="recipientAddress" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Recipient address{' '}
-                      </label>
-                      <input
-                        type="text"
-                        id="recipientAddress"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      />
-                    </div>
-                    <div className="mb-6">
-                      <div className="flex flex-row justify-between">
-                        <label htmlFor="Amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                          Amount{' '}
-                        </label>
-                        <label htmlFor="Amount" className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">
-                          Balance:111eth{' '}
-                        </label>
-                      </div>
+                        <div className="mb-6">
+                          <label htmlFor="recipientAddress" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Recipient address{' '}
+                          </label>
+                          <input
+                            type="text"
+                            id="recipientAddress"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          />
+                        </div>
+                        <div className="mb-6">
+                          <div className="flex flex-row justify-between">
+                            <label htmlFor="Amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                              Amount{' '}
+                            </label>
+                            <label htmlFor="Amount" className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">
+                              Balance:111eth{' '}
+                            </label>
+                          </div>
 
-                      <input
-                        type="text"
-                        id="Amount"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      />
-                    </div>
-                    <div className="mb-6 flex flex-col sm:flex-row justify-between gap-8">
-                      <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        Cancel
-                      </button>
-                      <button
-                        type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 "
-                      >
-                        next
-                      </button>
-                    </div>
-                  </div>
-                </Dialog.Panel>
+                          <input
+                            type="text"
+                            id="Amount"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          />
+                        </div>
+                        <div className="mb-6 flex flex-col sm:flex-row justify-between gap-8">
+                          <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            Previous
+                          </button>
+                          <button
+                            type="button"
+                            onClick={next}
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 "
+                          >
+                            next
+                          </button>
+                        </div>
+                      </div>
+                    </Dialog.Panel>
+                  </When>
+                  <When condition={isPreviewStep === true}>
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 flex  flex-row justify-between">
+                        <span className=" flex-1 ">Send Preview</span>{' '}
+                        <div className=" w-28 mx-2">
+                          <ChainName></ChainName>
+                        </div>
+                      </Dialog.Title>
+                      <div className="mt-4 flex flex-col  gap-1    w-96 ">
+                        <div className="mb-6 flex flex-row gap-2  items-center  justify-between text-center">
+                          <div>
+                            <label htmlFor="sendingfrom" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                              Sending from
+                            </label>
+                            <div className="flex flex-row  items-center">
+                              <Avvvatars value={'0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab'} style="shape" size={40} />
+                              <div className="break-all pl-2">0x12C...Aaab</div>
+                            </div>
+                          </div>
+                          <div>
+                            <label htmlFor="sendingfrom" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                              Recipient
+                            </label>
+                            <div className="flex flex-row items-center">
+                              <Avvvatars value={'0x12CF5132064Ee45AcD4843E8C9D7Ae5e3852Aaab'} style="shape" size={40} />
+                              <div className="break-all pl-2">0x12C...Aaab</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="mb-6">
+                          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Assert </label>
+                          <div className=" flex flex-row items-center gap-1">
+                            <img width={32} src={ethlogo}></img>
+                            <span>11.11 eth</span>
+                          </div>
+                        </div>
+                        <div className="mb-6">
+                          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Estimated fee </label>
+                          <div>0.001 eth</div>
+                        </div>
+                        <div className="mb-6">
+                          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Transaction validity </label>
+                          <div>Simulate</div>
+                        </div>
+
+                        <div className="mb-6 flex flex-col sm:flex-row justify-between gap-8">
+                          <button
+                            onClick={previous}
+                            className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                          >
+                            Previous
+                          </button>
+                          <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 "
+                          >
+                            next
+                          </button>
+                        </div>
+                      </div>
+                    </Dialog.Panel>
+                  </When>
+                </div>
               </Transition.Child>
             </div>
           </div>
