a495002a2585d99f3c543bb47e028416508e7b1c
zhangchao <544262408@qq.com>
2023-03-23 19:28:10 +0800

    feat: mobile menu

--- packages/ui-components/src/constants/rpcConfig.ts
+++ packages/ui-components/src/constants/rpcConfig.ts
@@ -1,5 +1,7 @@
+export const VITE_REACT_APP_MPCAPI = import.meta.env.VITE_REACT_APP_MPCAPI
+
 export const rpclist = [
-  'http://43.156.1.182:8888',
+  VITE_REACT_APP_MPCAPI,
   'http://localhost:5173/api',
   'http://43.153.80.95:5928',
   'http://43.157.49.23:5928',
--- packages/web/src/views/dashboard/sideBar.tsx
+++ packages/web/src/views/dashboard/sideBar.tsx
@@ -1,21 +1,29 @@
 import { useAppStore } from '@monorepo/ui-components'
-import { When } from 'react-if'
+import { Else, If, Then, When } from 'react-if'
 import UserPanel from '@monorepo/ui-components/src/components/siderBar/userPanel'
 import SiderMenu from '@monorepo/ui-components/src/components/siderBar/siderMenu'
 import ChainName from '@monorepo/ui-components/src/components/chainList/chainName'
-
+import WalletList from '@monorepo/ui-components/src/components/walletList/walletMenu'
 const SideBar = () => {
   const showsideBar = useAppStore(state => state.sideBar)
 
   const togglesideBar = useAppStore(state => state.togglesideBar)
+  const walletMenu = useAppStore(state => state.walletMenu)
+  const togglesidewalletMenu = useAppStore(state => state.togglesidewalletMenu)
 
   function onClick() {
     togglesideBar()
+    togglesidewalletMenu()
   }
 
   return (
     <div>
-      <aside
+      <If condition={walletMenu}>
+        <Then>
+          <WalletList></WalletList>
+        </Then>
+        <Else>
+        <aside
         id="logo-sidebar"
         className={`fixed  rounded-xl top-14 left-0 z-40 w-64 h-screen transition-transform ${showsideBar ? '' : '-translate-x-full sm:translate-x-0'}  `}
         aria-label="Sidebar"
@@ -26,6 +34,11 @@ const SideBar = () => {
           <SiderMenu></SiderMenu>
         </div>
       </aside>
+
+        </Else>
+
+      </If>
+      
       {/* <div className={`fixed  rounded-xl top-14 left-0 z-40 w-64 h-screen transition-transform ${showsideBar ? '' : '-translate-x-full sm:translate-x-0'}  `}>
       
       </div> */}
