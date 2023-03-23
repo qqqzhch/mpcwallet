5a2631f8039b3fda42cfdfed38f2f0831d6f1e41
zhangchao <544262408@qq.com>
2023-03-23 21:26:46 +0800

    feat: test

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import macrosPlugin from "vite-plugin-babel-macros"
 
-// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject';
 // import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
@@ -11,22 +11,50 @@ import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 import type * as http from 'node:http'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    // host:"0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "http://localhost:3004/",
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3004",
+          changeOrigin: true
+        },
       },
     },
-  },
-  plugins: [react(),
-  macrosPlugin(),
-  nodePolyfills()
-  ],
-  optimizeDeps: {
-    exclude: ['@ethersproject/hash', 'wrtc'],
-    include: ['js-sha3', '@ethersproject/bignumber']
+    plugins: [react(),
+    macrosPlugin(),
+    nodePolyfills()
+    ],
+    optimizeDeps: {
+      exclude: ['@ethersproject/hash', 'wrtc'],
+      include: ['js-sha3', '@ethersproject/bignumber'],
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+        plugins: [
+          
+        ]
+      }
+
+
+    },
+    define: {
+      global: 'globalThis',
+    },
+    resolve: {
+      alias: {
+        // Buffer : "buffer",
+
+      },
+    },
+    build: {
+
+      rollupOptions: {
+        plugins: [rollupNodePolyFill()],
+
+      },
+    }
   }
 })
