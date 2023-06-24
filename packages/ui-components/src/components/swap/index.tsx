877864c982ec4fd2ba2a69c6e1bb0ff93e6b748c
qqqzhch <qqqzhch@163.com>
2023-06-24 20:55:31 +0800

    feat: swap info

--- packages/ui-components/src/components/swap/index.tsx
+++ packages/ui-components/src/components/swap/index.tsx
@@ -19,11 +19,13 @@ import useErc20Approve from '../../hooks/useApprove'
 const Swap = () => {
   const [isFromOpen, setIsFromOpen] = useState(false)
   const [isToOpen, setIsToOpen] = useState(false)
-  const { account } = useWeb3React()
+  const { account,chainId } = useWeb3React()
   const [inputAmount,setInputAmount]=useState("0")
 
   const fromChainInfo= useAppStore((state)=>state.fromChain) 
   const toChainInfo = useAppStore((state)=>state.toChain)
+  const fromChainID = useAppStore((state)=>state.fromChainID)
+  const toChainID = useAppStore((state)=>state.toChainID)
   const USDCAddress = useUSDCAddress()
   const usdcBalance=  useErc20Balance(account,USDCAddress)
   const num =parseFloat(inputAmount)*1000000;
@@ -79,18 +81,18 @@ const Swap = () => {
           
         </div>
         <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="text"
-            name="output"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            output
+          
+          <label className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+          You will receive:xxx usdc
           </label>
         </div>
-        <div className=' flex'>
+        <div className="relative z-0 w-full mb-6 group">
+          
+          <label className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+          fee:xxx {fromChainInfo?.nativeCurrency.name}
+          </label>
+        </div>
+        <div className=' relative z-0 w-full mb-6 group flex mt-10'>
         <If condition={allowance}>
          <Then>
           
@@ -106,12 +108,29 @@ const Swap = () => {
 
          </Then>
          <Else>
-         <button
+          <If condition={fromChainID==chainId&&fromChainID!==toChainID}>
+            <Then>
+            <button
           
           className="text-white flex-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Swap
         </button>
+
+            </Then>
+            <Else>
+            <button
+          
+          className="text-white flex-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          {fromChainID!==toChainID?"need switch network":"The source and target network cannot be the same network "}
+         
+        </button>
+
+            </Else>
+
+          </If>
+         
 
          </Else>
         </If>
