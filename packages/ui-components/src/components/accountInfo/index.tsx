a3d107f061af800c8f681acb45571320787d0a05
qqqzhch <qqqzhch@163.com>
2023-06-25 09:35:21 +0800

    feat: text

--- packages/ui-components/src/components/accountInfo/index.tsx
+++ packages/ui-components/src/components/accountInfo/index.tsx
@@ -18,10 +18,7 @@ const ChainList:FC<Props> = ({children}) => {
                   {account?.substring(0,8)}...{account?.substring(30,42)}
                     
                   </div>
-                  <div className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
-                  {account?.substring(0,8)}...{account?.substring(30,42)}
-                    
-                  </div>
+                  
                   </div>
                 </div>
               </Popover.Panel>
