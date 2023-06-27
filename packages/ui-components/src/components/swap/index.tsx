9170903a04a3a3e4691fb13ed823f87add2bfb7c
qqqzhch <qqqzhch@163.com>
2023-06-27 13:28:59 +0800

    fix: config error

--- packages/ui-components/src/components/swap/index.tsx
+++ packages/ui-components/src/components/swap/index.tsx
@@ -114,6 +114,13 @@ const Swap = () => {
           <ArrowDownIcon  className="h-4 w-4 text-blue-500  "></ArrowDownIcon>
           
         </div>
+        <SwichNetwork></SwichNetwork>
+        <div onClick={()=>{setIsToOpen(true)}} className=" inline-flex  items-center  z-0 w-full   mb-6 group  pb-1  cursor-pointer">
+        <span className='peer-focus:font-medium  text-lg text-gray-500    min-w-[40%]' >To {toChainInfo?.label}  </span>  
+       
+          <ArrowDownIcon  className="h-4 w-4 text-blue-500  "></ArrowDownIcon>
+          
+        </div>
         <div className="relative z-0 w-full mb-6 group  flex flex-row">
           <div className=' flex-1'>
           <input
@@ -136,21 +143,16 @@ const Swap = () => {
           </label>
         </div>
         
-          <SwichNetwork></SwichNetwork>
         
-        <div onClick={()=>{setIsToOpen(true)}} className=" inline-flex  items-center  z-0 w-full   mb-6 group  pb-1  cursor-pointer">
-        <span className='peer-focus:font-medium  text-lg text-gray-500    min-w-[40%]' >To {toChainInfo?.label}  </span>  
+        
        
-          <ArrowDownIcon  className="h-4 w-4 text-blue-500  "></ArrowDownIcon>
-          
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
+        <div className="relative z-0 w-full  group  mb-14">
           
           <label className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
           You will receive:{inputAmount} USDC
           </label>
         </div>
-        <div className="relative z-0 w-full mb-6 group">
+        <div className="relative z-0 w-full  group mb-14">
           
           <label className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
           Fee:{fromChainID!==null&&formatUnits(fromChainID,RelayerFee,true) } 
